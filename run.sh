7b358a02592d8afca211aa6f37647ce1509e4731
tomeichlersmith <eichl008@umn.edu>
2022-11-14 12:23:09 -0600

    add sim option to run script

--- run.sh
+++ run.sh
@@ -6,10 +6,12 @@ __usage__() {
   cat <<HELP
  USAGE:
   ldmx ./run.sh [-o|--out-dir OUT_DIR] [-N|--n-events NUM_EVENTS]
+                [-s|--sim SIM]
 
  OPTIONS:
   -o, --out-dir  : Base output directory for data files
   -N, --n-events : Number of events per geometry to simulate (default: 10k)
+  -s, --sim      : Simulation to run (options: 'plain', 'ecalpn')
 
 HELP
 }
@@ -22,16 +24,33 @@ __main__() {
   fi
   local _output_dir=$(cd data && pwd -P)/dev
   local _n_events=10000
+  local _sim="plain"
   while [ $# -gt 0 ]; do
     case $1 in
-      -o|--out-dir|-N|--n-events)
+      -o|--out-dir|-N|--n-events|-s|--sim)
         if [ -z $2 ]; then
           echo "ERROR: '$1' requires an argument."
           return 1
         fi
         case $1 in
           -o|--out-dir) _output_dir=$2;;
-          -N|--n-events) _n_events=$2;;
+          -N|--n-events) 
+            if ! [[ $2 =~ '^[0-9]+$' ]]; then
+              echo "ERROR: '$2' is not a number."
+              return 1
+            fi
+            _n_events=$2;;
+          -s|--sim) 
+            case $2 in
+              plain|ecalpn)
+                _sim=$2
+                ;;
+              *)
+                echo "ERROR: Unrecognized simulation '$2'"
+                return 1
+                ;;
+            esac
+            ;;
         esac
         shift
         ;;
@@ -56,6 +75,7 @@ __main__() {
     fire simulation.py \
       --n-events ${_n_events} \
       --out-dir ${_output_dir} \
+      --sim ${_sim} \
       --geometry ${g} \
       &> ${_output_dir}/simulation_v${g}.log &
   done
