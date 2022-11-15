1d10add4d8356ecdea5afb8714d37b63462d16a5
tomeichlersmith <eichl008@umn.edu>
2022-11-15 12:55:17 -0600

    allow loading of a branch into a dataframe for later processing

--- comp/_differ.py
+++ comp/_differ.py
@@ -17,6 +17,7 @@ class FileEntry :
             self.__colmod = args[2]
         else :
             self.__colmod = None
+        self.__df = None
 
     def keys(self, *args, **kwargs) :
         """Callback into uproot keys
@@ -25,10 +26,49 @@ class FileEntry :
         what to plot within a notebook
         """
         return self.__file.keys(*args, **kwargs)
+    
+    def events(self, **kwargs) :
+        """Callback for retrieving a full in-memory data frame of the events
+        
+        All key-word arguments are passed to the uproot.arrays method.
+        
+        We change the default 'library' to be pandas which can be overridden
+        by a user if desired.
+        """
 
+        if 'library' not in kwargs :
+            kwargs['library'] = 'pd'
+        return self.__file['LDMX_Events'].arrays(**kwargs)
+    
+    def load(self, manipulation = None, **kwargs) :
+        """Instead of giving the events data frame to the caller,
+        we store the dataframe here for later batch processing
+        
+        manipulation is a function operating on the loaded dataframe
+        which is there for people to rename columns, calculate new
+        columns, etc...
+        
+        All the kwargs are simply provided to events for selecting
+        the branches of LDMX_Events to load into memory.
+        """
+        self.__df = self.events(**kwargs)
+        if manipulation is not None :
+            manipulation(self.__df)
+    
+    def manipulate(self, manipulation) :
+        """Apply the passed manipulation to the dataframe"""
+        manipulation(self.__df)
+    
     def plot1d(self, ax, obj_name, **hist_kwargs) :
         """Plot the input uproot object as a histogram on the input axes
 
+        If obj_name is not a str, we assume that it is a callable and will
+        provide the in-memory dataframe. An exception is thrown if the dataframe
+        has not been loaded.
+        
+        If the dataframe of events has been loaded and the obj_name is a
+        member of that dataframe, we use that dataframe to fill the histogram.
+        
         If the uproot_obj is already a histogram we import its values and use
         them directly. If the uproot_obj is a TBranch, then we pull its values
         into memory and fill the histogram.
@@ -42,7 +82,15 @@ class FileEntry :
             hist_kwargs['linewidth'] = 2
         if 'label' not in hist_kwargs :
             hist_kwargs['label'] = self.__name
+            
+        if not isinstance(obj_name, str) :
+                if self.__df is None :
+                        raise KeyError('Cannot use dynamic value calculations without loading the dataframe.')
+                return ax.hist(obj_name(self.__df), **hist_kwargs)
 
+        if self.__df is not None and obj_name in self.__df :
+                return ax.hist(self.__df[obj_name], **hist_kwargs)
+        
         if self.__colmod is not None :
             obj_name = self.__colmod(obj_name)
 
@@ -55,8 +103,7 @@ class FileEntry :
                 raise KeyError(f'Attempted to do a 1D plot of a {dim} dimension histogram.')
             return ax.hist((edges[1:]+edges[:-1])/2, bins=edges, weights=obj.values(), **hist_kwargs)
         else :
-            return ax.hist(obj.array(library='pd').values, **hist_kwargs)
-        
+            return ax.hist(obj.array(library='pd').values, **hist_kwargs)        
 
 class Differ :
     """Differ allowing easy comparison of "similar" files
@@ -123,3 +170,17 @@ class Differ :
                 fn = file_name
             fig.savefig(os.path.join(out_dir,fn)+'.pdf', bbox_inches='tight')
             fig.clf()
+
+    def load(self, **kwargs) :
+        """Load all of the event data frames into memory
+        
+        The key-word arguments are used in each FileEntry's events call
+        to specify which branches (if not all of them) should be loaded
+        into memory and what manipulation (if any) to do.
+        """
+        for f in self.files :
+            f.load(**kwargs)
+            
+    def manipulate(self, manipulation) :
+        for f in self.files :
+            f.manipulate(manipulation)
