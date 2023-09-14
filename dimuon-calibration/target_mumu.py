d5a2b436a4127c949acaafc3536b043820b6f543
tomeichlersmith <eichl008@umn.edu>
2023-09-14 11:35:14 -0500

    update config to specialize it more separate from Biasing/test

--- dimuon-calibration/target_mumu.py
+++ dimuon-calibration/target_mumu.py
@@ -2,6 +2,14 @@ import argparse
 import sys
 from pathlib import Path
 
+def nfmt(n):
+    """format an integer n with a nice suffix"""
+    for suffix in ['','k','M']:
+        if n < 1000:
+            return f'{n}{suffix}'
+        n //= 1000
+    return f'{n}B'
+
 parser = argparse.ArgumentParser(
     'ldmx fire '+sys.argv[0],
     formatter_class = argparse.ArgumentDefaultsHelpFormatter
@@ -13,35 +21,63 @@ parser.add_argument('--out-dir', type=Path, default=Path.cwd(), help='directory 
 args = parser.parse_args()
 
 from LDMX.Framework import ldmxcfg
-p = ldmxcfg.Process('target_mumu')
-from LDMX.Biasing import target
 from LDMX.SimCore import generators
+from LDMX.SimCore import simulator
+from LDMX.SimCore import bias_operators
+from LDMX.Biasing import filters
+from LDMX.Biasing import util
+from LDMX.Biasing import include as includeBiasing
+
+p = ldmxcfg.Process('target_mumu')
+
+includeBiasing.library()
 import LDMX.Ecal.EcalGeometry
 import LDMX.Hcal.HcalGeometry
 
-detector, beam = None, None
+detector, beam, energy = None, None, None
 if args.beam == '4':
     detector = 'ldmx-det-v14'
     beam = generators.single_4gev_e_upstream_tagger()
+    energy = 4000. #MeV
 elif args.beam == '8':
     detector = 'ldmx-det-v14-8gev'
     beam = generators.single_8gev_e_upstream_tagger()
+    energy = 8000. #MeV
 else:
     raise ValueError('--beam is not "4" or "8"! (probably bug in this config script)')
 
 if not args.out_dir.is_dir():
     raise ValueError(f'--out-dir {args.out_dir} does not exist (or it cant be seen in the container)')
 
-def nfmt(n):
-    """format an integer n with a nice suffix"""
-    for suffix in ['','k','M']:
-        if n < 1000:
-            return f'{n}{suffix}'
-        n //= 1000
-    return f'{n}B'
+sim = simulator.simulator("target_gammamumu")
+
+# Set the path to the detector to use.
+#   Also tell the simulator to include scoring planes
+sim.setDetector( detector , True )
+
+# Set run parameters
+sim.description = "gamma -> mu+ mu- in target region"
+sim.beamSpotSmear = [20., 80., 0.]
+sim.generators = [beam]
+
+min_photon_energy = 0.625*energy
+
+# Enable and configure the biasing
+sim.biasing_operators = [ 
+    bias_operators.GammaToMuPair('target', 1e6, min_photon_energy)
+]
+
+# Configure the sequence in which user actions should be called.
+sim.actions.extend([
+        # Only consider events where a hard brem occurs
+        filters.TaggerVetoFilter(thresh = 0.95*energy),
+        filters.TargetBremFilter(recoil_max_p = energy - min_photon_energy, brem_min_e = min_photon_energy),
+        filters.TargetGammaMuMuFilter(),
+        util.TrackProcessFilter.gamma_mumu()
+])
 
 p.run = args.run
-p.sequence = [ target.gamma_mumu(detector, beam) ]
+p.sequence = [ sim ]
 p.maxEvents = args.n_events
 p.maxTriesPerEvent = 1
 p.termLogLevel = 1
